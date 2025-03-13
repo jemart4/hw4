4c2d21c2b8398ceef2e8b7a3b314b2cabc3e804c
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-03-13 02:53:10 +0000

    uploading images works

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,14 +4,17 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    
   end
 
 end
